9f37d581513c61c1c3100be072f652c7de42939b
BetzaidaS <82418218+BetzaidaS@users.noreply.github.com>
2022-10-16 20:25:49 -0500

    pagina principal del solicitante
    y carpetea de img reemplazar por esta

---
+++ proyectoattutp/C-PaginaPrincipal.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" href="" />
+  <title>navegacion</title>
+  <!-- logo-->
+  <link rel="icon" type="image/x-icon" href="../img/tropical_utp_logo-modified.png" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+  <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+  <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+  <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+  <link href="../Css/estilo1.css" rel="stylesheet">
+  <link href="../js/carrusel.js" rel="stylesheet">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link href="https://raw.githubusercontent.com/daneden/animate.css/master/animate.css" rel="stylesheet">
+</head>
+
+<body>
+  <header>
+    <div>
+      <nav class="navbar navbar-light py-0 padding-top:0">
+        <a class="navbar-brand" href="#">
+          <img src="../img/tropical_utp_logo.jpg" class="img-circle" width="100" height="100"
+            class="d-inline-block align-top" alt="">
+          Universidad Tecnológica de Panamá</a>
+        <div class="navbar-nav">
+          <a class="nav-item nav-link" href="#">Cerrar Sesion</a>
+        </div>
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color:#68086c;"
+      class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
+      <a class="navbar-brand py-0" href="#">Servicio de Solicitud de Vehiculos</a>
+      <ul class="menu">
+        <li class="nav-item"><a class="nav-link" href="#">Inicio</a></li>
+        <li class="nav-item"><a class="nav-menu-link nav-link nav-menu-link_active" href="#">Solicitud</a></li>
+        <li class="nav-item"><a class="nav-link" href="">Estado De
+            Solicitud</a></li>
+      </ul>
+      </div>
+    </nav>
+    </div>
+    </nav>
+  </header>
+
+
+  <div id="myCarousel" class="carousel slide carousel-fade" data-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <div class="mask flex-center">
+          <div class="container">
+            <div class="row align-items-center">
+              <div class="col-md-7 col-12 order-md-1 order-2">
+                <br>
+                <br>
+                <h4>Solicitud de vehículos<br>
+                </h4>
+                <br>
+                <p>La universidad tecnológica de Panamá cuenta con una flota de vehículos a disposición de ustedes
+                  Para transportar al personal administrativo, investigativo y estudiantil a los diferentes puntos que
+                  lo requieran.</p>
+                <a href="#">IR</a>
+              </div>
+              <br>
+              <br>
+              <div class="col-md-5 col-12 order-md-2 order-1"><img src="../img/AUTO4-removebg-preview.png"
+                  class="mx-auto" alt="slide"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <div class="mask flex-center">
+          <div class="container">
+            <div class="row align-items-center">
+              <div class="col-md-7 col-12 order-md-1 order-2">
+                <br>
+                <br>
+                <h4><br>
+                  Para solicitar</h4>
+                <br>
+                <p> Debes llenar el formulario de solicitud de vehículo luego esperar que
+                  esta este aprobada con
+                  el vehículo y conductor correspondientes.</p>
+                <a href="#">IR</a>
+              </div>
+              <br>
+              <br>
+              <div class="col-md-5 col-12 order-md-2 order-1"><img src="../img/auto1.png" class="mx-auto" alt="slide">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <div class="mask flex-center">
+          <div class="container">
+            <div class="row align-items-center">
+              <div class="col-md-7 col-12 order-md-1 order-2">
+                <br>
+                <br>
+                <h4><br>
+                  Recomendación</h4>
+                <br>
+                <p>Llena el formulario con cuidado y consciente de los horarios por si necesitas solicitar salvo
+                  conducto.</p>
+                <a href="#">IR</a>
+              </div>
+              <br>
+              <br>
+              <div class="col-md-5 col-12 order-md-2 order-1"><img src="../img/AUTO3-rtemovebg-preview.png"
+                  class="mx-auto" alt="slide"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!--slide end-->
+    <div>
+    </div>
+
+    <div class="py-4" style="background-color:#ffffff" id="">
+      <div class="center">
+        <img src="../img/banner3.png" higth="100" width="1200px">
+      </div>
+    </div>
+
+    <div class="py-4" style="background-color:#ffffff" id="">
+
+
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-6 col-md-4 col-lg-3 mt-4">
+            <div class="card card-inverse card-info">
+              <img class="card-img-top" src="../img/viajecolon.jpg">
+              <div class="card-block">
+                <h4 class="card-title">Nuevo Autobus</h4>
+                <div class="meta card-text">
+                  <a>Nuevo Autobus</a>
+                </div>
+                <div class="card-text">
+                  Con una inversión de más de 160 mil dólares, la Universidad Tecnológica de Panamá (UTP), adquirió un
+                  autobús de 44
+                  pasajeros, para el Centro Regional de Colón.
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-4 col-lg-3 mt-4">
+            <div class="card card-inverse card-info">
+              <img class="card-img-top" src="../img/GIRAS.jfif">
+              <div class="card-block">
+                <h4 class="card-title">Gira en Colón</h4>
+                <div class="meta card-text">
+                  <a>Gira en Colón</a>
+                </div>
+                <div class="card-text">
+                  Una gira a varias instalaciones de la Universidad Tecnológica de Panamá (UTP), en el área de
+                  Espinar,
+                  en la provincia de
+                  Colón.
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-4 col-lg-3 mt-4">
+            <div class="card" style="width: 41rem; height: 33rem; border-color: #68086c;">
+              <div class="card-header" style="background-color: #68086c; color: white;">
+                <h3> Novedades</h2>
+              </div>
+              <div class="card-body">
+                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                  <div class="carousel-inner">
+                    <div class="carousel-item active">
+                      <img class="d-block w-35" src="../img/AUTO4.jpeg" alt="First slide" higth="200" width="600px">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-35" src="../img/AUTO3.jpeg" alt="Second slide" higth="200" width="600px">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <br><br>
+          </div>
+
+        </div>
+      </div>
+    </div>
+    <br>
+    <br>
+
+    <div class="py-4" style="background-color:#ffffff" id="">
+      <div class="center">
+        <img src="../img/banner4.png" higth="100" width="1200px">
+      </div>
+    </div>
+    </center>
+
+    <footer>
+      <?php
+        require_once('../complementos/fotter.php');
+        ?>
+    </footer>
+</body>
+
+</html>
